dd13a11fa30fe75ea5b412e78ef8ef62c73fc89e
EDGI0081 <edgija@gmail.com>
2021-05-22 10:41:49 -0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+ import {interaction, sendMsg} from '../scripts/mqtt.js'
+ let topicX = 'elTopicEDGI';
+ let msgX = 'Hola Edgi';
+
+ $: console.log('Test from app.svelte: ' + interaction.msg);
+</script>
+
+<div>
+	<h2>Test MQTT</h2>
+</div>
+	<div>
+		<p>Topic: {$interaction.topic}</p>
+
+	</div>
+<div>
+	<div>
+		<p>Message: {$interaction.msg}</p>
+	</div>
+</div>
+
+<button type='button' on:click={() => sendMsg(topicX,msgX)}>SEND MESSAGE</button>
+
